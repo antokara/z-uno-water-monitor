d4e86237172d5ad767f97d4834b90959194981dc
Antonios Karagiannis <antokarag@gmail.com>
2022-07-01 23:46:08 -0600

    fix precision

--- src/monitor.ino
+++ src/monitor.ino
@@ -1,62 +1,58 @@
+#define PULSE_SENSOR_PIN 12
+
 // Normal Flow Range: 0.25 - 15 GPM
 // Pulse Rate is 1 Pulse/Gallon
 ZUNO_SETUP_CHANNELS(
     ZUNO_SENSOR_MULTILEVEL(ZUNO_SENSOR_MULTILEVEL_TYPE_GENERAL_PURPOSE_VALUE,
                            SENSOR_MULTILEVEL_SCALE_DIMENSIONLESS_VALUE,
-                           SENSOR_MULTILEVEL_SIZE_FOUR_BYTES,
-                           SENSOR_MULTILEVEL_PRECISION_ONE_DECIMAL,
-                           &getterFunction),
-    ZUNO_SENSOR_MULTILEVEL(ZUNO_SENSOR_MULTILEVEL_TYPE_GENERAL_PURPOSE_VALUE,
-                           SENSOR_MULTILEVEL_SCALE_DIMENSIONLESS_VALUE,
                            SENSOR_MULTILEVEL_SIZE_TWO_BYTES,
-                           SENSOR_MULTILEVEL_PRECISION_ONE_DECIMAL,
-                           &getterFunction2),
-    ZUNO_SENSOR_MULTILEVEL(ZUNO_SENSOR_MULTILEVEL_TYPE_GENERAL_PURPOSE_VALUE,
-                           SENSOR_MULTILEVEL_SCALE_DIMENSIONLESS_VALUE,
-                           SENSOR_MULTILEVEL_SIZE_ONE_BYTE,
-                           SENSOR_MULTILEVEL_PRECISION_ONE_DECIMAL,
-                           &getterFunction3));
-
+                           SENSOR_MULTILEVEL_PRECISION_TWO_DECIMALS,
+                           &getterFunction));
 int lastPulse;
-word gpm;
+int pulseSensorValue = LOW;
+float gpm = 0.0;
 
 void setup()
 {
+    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(PULSE_SENSOR_PIN, INPUT_PULLUP);
+    Serial.begin(9600);
 }
 
 void loop()
 {
+    pulseSensorValue = digitalRead(PULSE_SENSOR_PIN);
+    if (pulseSensorValue == LOW)
+    {
+        gpm = 15.0;
+        digitalWrite(LED_BUILTIN, HIGH);
+    }
+    else
+    {
+        gpm = 0.25;
+        digitalWrite(LED_BUILTIN, LOW);
+    }
+    Serial.print("gpm: ");
+    Serial.println(gpm);
+
+    // According to Z-Wave Plus restrictions, values from Sensor Multilevel channels
+    // (defined via ZUNO_SENSOR_MULTILEVEL macro) will not be sent unsolicitedly
+    // to Life Line more often than every 30 seconds.
     zunoSendReport(1);
-    delay(10000);
+    delay(1000);
 }
 
 /**
  * @brief
- *
- * getter function works for floats whereas variables, so far, only work for integer values.
- *
- * @return float
+ * @return
  */
 word getterFunction()
 {
-    // 1.0 = 106535321.6
-    // 2.0 = 107374182.4    838860.8
-    // 3.0 = 107793612.8    419430.4
-    return 3.0;
-}
-
-word getterFunction2()
-{
-    // 1.0 = 106535321.6
-    // 2.0 = 107374182.4    838860.8
-    // 3.0 = 107793612.8    419430.4
-    return 3.0;
-}
-
-word getterFunction3()
-{
-    // 1.0 = 106535321.6
-    // 2.0 = 107374182.4    838860.8
-    // 3.0 = 107793612.8    419430.4
-    return 3.0;
+    if (gpm > 0.0)
+    {
+        // for SENSOR_MULTILEVEL_PRECISION_ONE_DECIMAL we need * 10
+        // for SENSOR_MULTILEVEL_PRECISION_TWO_DECIMALS we need * 100
+        return gpm * 100.0;
+    }
+    return 0.0;
 }
