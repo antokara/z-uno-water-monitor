640c51d869d4616455846e134e39945fbc8fbf8c
Antonios Karagiannis <antokarag@gmail.com>
2022-07-02 22:07:06 -0600

    add gpm calculation and timeout

--- src/monitor.ino
+++ src/monitor.ino
@@ -1,7 +1,35 @@
+// the pin that we need to connect the water meter pulse switch.
+// the other end, needs to go the ground (GND) pin, of zuno.
 #define PULSE_SENSOR_PIN 12
+
+// minimum gallons per minute that the water meter can detect.
+// this helps us detect no-flow, by calculating a "time-out" when
+// too much time has passed since a new pulse.
+#define MIN_GPM 0.1
+
+// time in milliseconds for our target rate
+// 60000msecs = 60secs = 1minute rate for GPM
+#define TARGET_RATE_TIME 60000.0
+
+// number of pulses per gallon (Pulse/Gallon)
+#define PULSE_RATE 1.0
 
 // holds the last pulse sensor isActive
 boolean lastPulseSensorIsActive = false;
+
+// formula for getting GPM, using pulse rate and duration between pulses
+// [target rate time] / [duration between pulses] / [pulse rate] = Gallons Per Rate
+// examples:
+// target rate: 60 seconds to get (GPM)
+// pulse rate: 1
+// 60secs / 60secs / 1 pulse per gallon = 1 GPM
+// 60 / 120 / 1 = 0.5
+// pulse rate: 2
+// 60 / 60 / 2 = 0.5
+// 60 / 30 / 2 = 1
+
+// formula for getting duration between pulses, using target rate time, Gallons Per Rate and pulse rate
+// [target rate time] / [Gallons Per Rate] / [pulse rate] = [duration between pulses]
 
 // Normal Flow Range: 0.25 - 15 GPM
 // Pulse Rate is 1 Pulse/Gallon
@@ -13,10 +41,16 @@ ZUNO_SETUP_CHANNELS(
                            &getterFunction));
 unsigned long lastPulseTime = 0;
 
+// current gallons per minute
 float gpm = 0.0;
+
+float flowTimeout = 0;
 
 void setup()
 {
+    // calculate how much time must pass without a pulse, in order to consider no-flow
+    flowTimeout = TARGET_RATE_TIME / MIN_GPM / PULSE_RATE;
+
     pinMode(LED_BUILTIN, OUTPUT);
     pinMode(PULSE_SENSOR_PIN, INPUT_PULLUP);
     Serial.begin(9600);
@@ -26,22 +60,33 @@ void loop()
 {
     if (isPulseSensorActive())
     {
-        gpm = 15.0;
+        // we got a pulse (this can only happen once, per pulse,
+        // even if the meter stops right when the switch is on and the switch remains on)
+        gpm = TARGET_RATE_TIME / timePassedSinceLastPulse() / PULSE_RATE;
         digitalWrite(LED_BUILTIN, HIGH);
+        sendData();
     }
-    else
+    else if (timePassedSinceLastPulse() > flowTimeout)
     {
-        gpm = 0.25;
+        // flow timed-out. consider it now that there's no flow
+        gpm = 0.0;
         digitalWrite(LED_BUILTIN, LOW);
+        sendData();
     }
+}
+
+/**
+ * @brief sends the zwave data for all the channels
+ *
+ * According to Z-Wave Plus restrictions, values from Sensor Multilevel channels
+ * (defined via ZUNO_SENSOR_MULTILEVEL macro) will not be sent unsolicitedly
+ * to Life Line more often than every 30 seconds.
+ */
+void sendData()
+{
     Serial.print("gpm: ");
     Serial.println(gpm);
-
-    // According to Z-Wave Plus restrictions, values from Sensor Multilevel channels
-    // (defined via ZUNO_SENSOR_MULTILEVEL macro) will not be sent unsolicitedly
-    // to Life Line more often than every 30 seconds.
     zunoSendReport(1);
-    delay(1000);
 }
 
 /**
@@ -59,8 +104,12 @@ bool isPulseSensorActive()
         // when the sensor is in active state
         if (!lastPulseSensorIsActive)
         {
-            // it just turned active
+            // and it just turned active
             lastPulseSensorIsActive = true;
+
+            // reset the timer
+            lastPulseTime = millis();
+
             // only the first time, return true
             return lastPulseSensorIsActive;
         }
@@ -78,7 +127,7 @@ bool isPulseSensorActive()
 /**
  * @brief
  *
- * @return unsigned long milliseconds since the last pulse
+ * @return milliseconds since the last pulse
  */
 unsigned long timePassedSinceLastPulse()
 {
