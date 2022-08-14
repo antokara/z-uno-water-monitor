ff0cb330afae9f47c59ebe34eaba1f103fe9c222
Antonios Karagiannis <antokarag@gmail.com>
2022-08-14 14:18:06 -0600

    add gallon counter and pressure send debouncer

--- src/monitor.ino
+++ src/monitor.ino
@@ -1,9 +1,16 @@
+/**
+ * According to Z-Wave Plus restrictions, values from Sensor Multilevel channels
+ * (defined via ZUNO_SENSOR_MULTILEVEL macro) will not be sent unsolicitedly
+ * to Life Line more often than every 30 seconds.
+ */
+#define SEND_DATA_FREQUENCY 30000
+
 #include "pressureSensor.h"
 #include "pulseSensor.h"
 
 // setup the z-wave channels
 ZUNO_SETUP_CHANNELS(
-    // channel 1 - water flow
+    // channel 1 - water flow (in GPM)
     ZUNO_SENSOR_MULTILEVEL(ZUNO_SENSOR_MULTILEVEL_TYPE_GENERAL_PURPOSE_VALUE,
                            SENSOR_MULTILEVEL_SCALE_DIMENSIONLESS_VALUE,
                            SENSOR_MULTILEVEL_SIZE_TWO_BYTES,
@@ -14,9 +21,13 @@ ZUNO_SETUP_CHANNELS(
                            SENSOR_MULTILEVEL_SCALE_DIMENSIONLESS_VALUE,
                            SENSOR_MULTILEVEL_SIZE_TWO_BYTES,
                            SENSOR_MULTILEVEL_PRECISION_TWO_DECIMALS,
-                           &getPSI))
-// channel 3 - water meter pulse (per gallon)
-// ZUNO_SENSOR_BINARY_GENERAL_PURPOSE(&getWaterMeterPulse),
+                           &getPSI),
+    // channel 3 - gallons counter (to increase the water meter/counter)
+    ZUNO_SENSOR_MULTILEVEL(ZUNO_SENSOR_MULTILEVEL_TYPE_GENERAL_PURPOSE_VALUE,
+                           SENSOR_MULTILEVEL_SCALE_DIMENSIONLESS_VALUE,
+                           SENSOR_MULTILEVEL_SIZE_TWO_BYTES,
+                           SENSOR_MULTILEVEL_PRECISION_TWO_DECIMALS,
+                           &getGallonsCounter));
 
 // enable S2 authenticated mode
 ZUNO_SETUP_S2ACCESS(SKETCH_FLAG_S2_AUTHENTICATED_BIT);
@@ -57,12 +68,12 @@ word getPSI()
 }
 
 /**
- * @brief Get the Water Meter Pulse
- *
- * @return true
- * @return false
+ * @brief the number of gallons to increase the water meter counter by.
+ *        the goal is to only return a positive number, when at least one gallon has been counted.
+ *        if more than 1 GPM have been counted, we will instead send that total number.
+ * @return
  */
-bool getWaterMeterPulse()
+word getGallonsCounter()
 {
-    return lastPulseSensorIsActive;
+    return gallonsCounter * 100.0;
 }
